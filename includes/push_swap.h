98969b43e51e143092890a4cd38d6c57b8da0746
Kotaro666-dev <kotaro0726@gmail.com>
2021-03-11 08:50:05 +0900

    Update: Include libft header

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,10 +6,9 @@
 /*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/11 08:00:49 by kkamashi          #+#    #+#             */
-/*   Updated: 2021/03/11 08:02:34 by kkamashi         ###   ########.fr       */
+/*   Updated: 2021/03/11 08:45:14 by kkamashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
@@ -18,5 +17,6 @@
 # include <stdlib.h>
 # include <fcntl.h>
 # include <stdio.h>
+# include "libft.h"
 
 #endif
--- includes/utils.h
+++ includes/utils.h
@@ -1,22 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   checker.h                                          :+:      :+:    :+:   */
+/*   utils.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/03/11 08:00:14 by kkamashi          #+#    #+#             */
-/*   Updated: 2021/03/11 08:45:21 by kkamashi         ###   ########.fr       */
+/*   Created: 2021/03/11 08:44:25 by kkamashi          #+#    #+#             */
+/*   Updated: 2021/03/11 08:49:35 by kkamashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CHECKER_H
-# define CHECKER_H
+#ifndef UTILS_H
+# define UTILS_H
 
 # include <unistd.h>
 # include <stdlib.h>
 # include <fcntl.h>
 # include <stdio.h>
 # include "libft.h"
+
+void		print_out_ok(void);
+void		print_out_ko(void);
 
 #endif
