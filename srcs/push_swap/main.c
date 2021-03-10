d35e7bf9ce6efd26a3d2bd2b5dfffea0432d9415
Kotaro666-dev <kotaro0726@gmail.com>
2021-03-11 08:07:57 +0900

    Update: Add srcs of checker and push_swao

---
+++ srcs/push_swap/main.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/11 08:04:09 by kkamashi          #+#    #+#             */
+/*   Updated: 2021/03/11 08:05:05 by kkamashi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	(void)argv;
+	printf("This is push_swap program");
+	return (0);
+}
