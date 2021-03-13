389a3b353848d206279b5694e32741d9b287f923
Kotaro666-dev <kotaro0726@gmail.com>
2021-03-14 08:30:51 +0900

    Update: Update header file

--- includes/utils.h
+++ includes/utils.h
@@ -6,7 +6,7 @@
 /*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/11 08:44:25 by kkamashi          #+#    #+#             */
-/*   Updated: 2021/03/12 14:43:28 by kkamashi         ###   ########.fr       */
+/*   Updated: 2021/03/14 08:28:12 by kkamashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,19 +19,29 @@
 # include <stdio.h>
 # include "libft.h"
 # include "constant.h"
+# include "struct.h"
+# include "get_next_line.h"
 
 /*
 ** PRINT_OUT.C
 */
 
-void		print_out_ok(void);
-void		print_out_ko(void);
-void		print_out_error(void);
+int			print_out_ok(void);
+int			print_out_ko(void);
+int			print_out_error(void);
 
 /*
 ** UTILITIES.C
 */
 
 int		is_arg_numeric(char *arg);
+int		is_stack_empty(t_stack *stack);
+int		is_stack_sorted(t_stack *stack);
+
+/*
+**	READ_AND_OPERATE_COMMANDS.C
+*/
+
+int		read_and_operate_commands(t_checker *checker);
 
 #endif
