88f4cafda616283aea7cc23608d77ec6b41b9e98
Kotaro666-dev <kotaro0726@gmail.com>
2021-03-14 08:30:10 +0900

    Update: Add new utilitie files

--- srcs/utils/utilites.c
+++ srcs/utils/utilites.c
@@ -6,7 +6,7 @@
 /*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/12 14:40:33 by kkamashi          #+#    #+#             */
-/*   Updated: 2021/03/12 15:18:13 by kkamashi         ###   ########.fr       */
+/*   Updated: 2021/03/14 08:27:40 by kkamashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,35 @@ int		is_arg_numeric(char *arg)
 			return (FALSE);
 		}
 		i++;
+	}
+	return (TRUE);
+}
+
+int		is_stack_empty(t_stack *stack)
+{
+	if (stack == NULL)
+	{
+		return (TRUE);
+	}
+	return (FALSE);
+}
+
+int		is_stack_sorted(t_stack *stack)
+{
+	t_stack *current;
+
+	if (stack == NULL)
+	{
+		return (TRUE);
+	}
+	current = stack;
+	while (current->next)
+	{
+		if (current->value > current->next->value)
+		{
+			return (FALSE);
+		}
+		current = current->next;
 	}
 	return (TRUE);
 }
