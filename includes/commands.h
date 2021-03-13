f2d1548498dfb9e14bed257c807c898099964521
Kotaro666-dev <kotaro0726@gmail.com>
2021-03-13 21:26:25 +0900

    Update: Add commands header

---
+++ includes/commands.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   commands.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/13 21:22:16 by kkamashi          #+#    #+#             */
+/*   Updated: 2021/03/13 21:22:41 by kkamashi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef COMMANDS_H
+# define COMMANDS_H
+
+# define SUCCESS 0
+
+# include "checker.h"
+# include "struct.h"
+
+#endif
