ac9acaafa933773ce5d79d52c9192a89968dfb6e
Kotaro666-dev <kotaro0726@gmail.com>
2021-03-14 09:18:51 +0900

    Update: Update debug function

--- srcs/debug/debug.c
+++ srcs/debug/debug.c
@@ -6,21 +6,20 @@
 /*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/12 14:44:50 by kkamashi          #+#    #+#             */
-/*   Updated: 2021/03/12 14:52:35 by kkamashi         ###   ########.fr       */
+/*   Updated: 2021/03/14 09:10:59 by kkamashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "debug.h"
 
-void	print_out_stack_a(t_stack *stack_a)
+void	print_out_stack(t_stack *stack)
 {
 	t_stack *current;
 
-	current = stack_a;
-	while (current->next)
+	current = stack;
+	while (current)
 	{
 		printf("%d\n", current->value);
 		current = current->next;
 	}
-	printf("%d\n", current->value);
 }
