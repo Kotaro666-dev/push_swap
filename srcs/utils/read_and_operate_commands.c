830726261e7e4cc829fe48dd0ffba87479de572d
Kotaro666-dev <kotaro0726@gmail.com>
2021-03-16 22:12:21 +0900

    Update: Remove curly braces from conditinal case

--- srcs/utils/read_and_operate_commands.c
+++ srcs/utils/read_and_operate_commands.c
@@ -6,7 +6,7 @@
 /*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/14 07:43:33 by kkamashi          #+#    #+#             */
-/*   Updated: 2021/03/16 17:49:46 by kkamashi         ###   ########.fr       */
+/*   Updated: 2021/03/16 18:28:33 by kkamashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,54 +15,29 @@
 int		operate_commands(char *line, t_checker *checker)
 {
 	if (!ft_strcmp(line, "sa"))
-	{
 		swap_a(&(checker->stack_a));
-	}
 	else if (!ft_strcmp(line, "sb"))
-	{
 		swap_b(&(checker->stack_b));
-	}
 	else if (!ft_strcmp(line, "ss"))
-	{
 		swap_a_and_b(checker);
-	}
 	else if (!ft_strcmp(line, "pa"))
-	{
 		push_a(&(checker->stack_a), &(checker->stack_b));
-	}
 	else if (!ft_strcmp(line, "pb"))
-	{
 		push_b(&(checker->stack_a), &(checker->stack_b));
-	}
 	else if (!ft_strcmp(line, "ra"))
-	{
 		rotate_a(&(checker->stack_a));
-	}
 	else if (!ft_strcmp(line, "rb"))
-	{
 		rotate_b(&(checker->stack_b));
-	}
 	else if (!ft_strcmp(line, "rr"))
-	{
 		rotate_a_and_b(checker);
-	}
 	else if (!ft_strcmp(line, "rra"))
-	{
 		reverse_rotate_a(&(checker->stack_a));
-	}
 	else if (!ft_strcmp(line, "rrb"))
-	{
 		reverse_rotate_b(&(checker->stack_b));
-	}
 	else if (!ft_strcmp(line, "rrr"))
-	{
 		reverse_rotate_a_and_b(checker);
-	}
 	else
-	{
 		return (ERROR);
-	}
-	print_out_stack(checker->stack_a, checker->stack_b);
 	return (SUCCESS);
 }
 
@@ -82,18 +57,13 @@ int		read_and_operate_commands(t_checker *checker)
 			break ;
 		}
 		else if (return_value == GNL_ERROR)
-		{
 			return (ERROR);
-		}
 		formatted_line = ft_strtrim(line, " ");
 		if (!formatted_line)
-		{
 			return (ERROR);
-		}
 		if (operate_commands(formatted_line, checker) == ERROR)
-		{
 			return (ERROR);
-		}
+		print_out_stack(checker->stack_a, checker->stack_b);
 		free(line);
 		free(formatted_line);
 	}
