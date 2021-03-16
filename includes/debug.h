9d4d5c4307a344f42d96e23f0fa54d980be1515a
Kotaro666-dev <kotaro0726@gmail.com>
2021-03-16 15:55:26 +0900

    Update: Update debug function to print out stack

--- includes/debug.h
+++ includes/debug.h
@@ -6,7 +6,7 @@
 /*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/12 14:45:25 by kkamashi          #+#    #+#             */
-/*   Updated: 2021/03/14 09:10:48 by kkamashi         ###   ########.fr       */
+/*   Updated: 2021/03/16 15:51:22 by kkamashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@
 # include "checker.h"
 # include "struct.h"
 
-void	print_out_stack(t_stack *stack);
+void	print_out_stack(t_stack *stack_a, t_stack *stack_b);
 
 #endif
--- srcs/debug/debug.c
+++ srcs/debug/debug.c
@@ -6,20 +6,34 @@
 /*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/12 14:44:50 by kkamashi          #+#    #+#             */
-/*   Updated: 2021/03/14 09:10:59 by kkamashi         ###   ########.fr       */
+/*   Updated: 2021/03/16 15:54:07 by kkamashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "debug.h"
 
-void	print_out_stack(t_stack *stack)
+void	print_out_stack(t_stack *stack_a, t_stack *stack_b)
 {
-	t_stack *current;
+	t_stack *current_a;
+	t_stack *current_b;
 
-	current = stack;
-	while (current)
+	current_a = stack_a;
+	current_b = stack_b;
+	printf("_ _\n");
+	while (current_a || current_b)
 	{
-		printf("%d\n", current->value);
-		current = current->next;
+		if (current_a)
+		{
+			printf("%d ", current_a->value);
+			current_a = current_a->next;
+		}
+		if (current_b)
+		{
+			printf("%d ", current_b->value);
+			current_b = current_b->next;
+		}
+		printf("\n");
 	}
+	printf("_ _\n");
+	printf("a b\n");
 }
