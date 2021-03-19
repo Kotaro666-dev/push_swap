0880c9024d72a70a2a36eed60d8fda1a92bc630b
Kotaro666-dev <kotaro0726@gmail.com>
2021-03-19 17:01:10 +0900

    Modify: make funciton static

--- includes/utils.h
+++ includes/utils.h
@@ -6,7 +6,7 @@
 /*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/11 08:44:25 by kkamashi          #+#    #+#             */
-/*   Updated: 2021/03/17 07:48:00 by kkamashi         ###   ########.fr       */
+/*   Updated: 2021/03/19 15:00:52 by kkamashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,7 +59,6 @@ void		free_memory(t_stack **stack_a, t_stack **stack_b);
 */
 
 void		initialize_struct(t_main *main);
-int			store_value_in_stack_a(t_stack **stack_a, int num);
 int			initialize_stack_a(t_main *main, char **argv);
 
 #endif
