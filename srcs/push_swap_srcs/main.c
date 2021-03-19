8c22c06ddc1f0c53d28f67f65babbdb1a16c84f0
Kotaro666-dev <kotaro0726@gmail.com>
2021-03-19 14:04:19 +0900

    Update: Add sort funcitons for size == 2 and 3

--- srcs/push_swap_srcs/main.c
+++ srcs/push_swap_srcs/main.c
@@ -6,11 +6,78 @@
 /*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/11 08:04:09 by kkamashi          #+#    #+#             */
-/*   Updated: 2021/03/17 07:49:07 by kkamashi         ###   ########.fr       */
+/*   Updated: 2021/03/19 14:02:34 by kkamashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
+
+void		sort_stack_a_by_size_is_two(t_main *main)
+{
+	t_stack **stack_a;
+
+	stack_a = &(main->stack_a);
+	if ((*stack_a)->value < (*stack_a)->next->value)
+	{
+		return ;
+	}
+	swap_a(main);
+}
+
+void		sort_stack_a_by_size_is_three(t_main *main)
+{
+	t_stack **stack_a;
+
+	stack_a = &(main->stack_a);
+	main->top = (*stack_a)->value;
+	main->middle = (*stack_a)->next->value;
+	main->bottom = (*stack_a)->next->next->value;
+	if (main->top > main->middle && main->top < main->bottom)
+	{
+		swap_a(main);
+	}
+	else if (main->top > main->middle && main->middle > main->bottom)
+	{
+		swap_a(main);
+		reverse_rotate_a(main);
+	}
+	else if (main->top > main->middle && main->middle < main->bottom)
+	{
+		rotate_a(main);
+	}
+	else if (main->top < main->middle && main->top < main->bottom && main->middle > main->bottom)
+	{
+		swap_a(main);
+		rotate_a(main);
+	}
+	else if (main->top < main->middle && main->top > main->bottom && main->middle > main->bottom)
+	{
+		reverse_rotate_a(main);
+	}
+}
+
+void		sort_stack_a_by_size(t_main *main)
+{
+	if (main->size < 2)
+		return ;
+	else if (main->size == 2)
+	{
+		sort_stack_a_by_size_is_two(main);
+	}
+	else if (main->size == 3)
+	{
+		sort_stack_a_by_size_is_three(main);
+	}
+	else if (main->size <= 5)
+	{
+
+	}
+	else
+	{
+
+	}
+}
+
 
 int			main(int argc, char *argv[])
 {
@@ -30,6 +97,7 @@ int			main(int argc, char *argv[])
 		return (print_out_error());
 	}
 	// SORT
+	sort_stack_a_by_size(&main);
 	// if (!is_stack_sorted(main.stack_a))
 	// {
 	// 	return (print_out_ko());
@@ -38,6 +106,8 @@ int			main(int argc, char *argv[])
 	// {
 	// 	return (print_out_ko());
 	// }
+	print_out_stack(main.stack_a, main.stack_b);
+	print_out_count(&main);
 	free_memory(&(main.stack_a), &(main.stack_b));
 	return (print_out_ok());
 }
