bdf9916ad68f72364b158296cdb54c93159c9932
Kotaro666-dev <kotaro0726@gmail.com>
2021-03-19 17:01:31 +0900

    Update: Update main.c

--- includes/main.h
+++ includes/main.h
@@ -6,7 +6,7 @@
 /*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/11 08:00:14 by kkamashi          #+#    #+#             */
-/*   Updated: 2021/03/16 22:47:20 by kkamashi         ###   ########.fr       */
+/*   Updated: 2021/03/19 16:55:14 by kkamashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,13 @@
 # include "utils.h"
 # include "debug.h"
 # include "struct.h"
+
+/*
+** SORTING FUNCTION
+*/
+
+void		sort_stack_with_two_numbers(t_main *main);
+void		sort_stack_with_three_numbers(t_main *main);
+void		sort_stack_with_more_than_four_numbers(t_main *main);
 
 #endif
--- srcs/push_swap_srcs/main.c
+++ srcs/push_swap_srcs/main.c
@@ -6,55 +6,11 @@
 /*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/11 08:04:09 by kkamashi          #+#    #+#             */
-/*   Updated: 2021/03/19 14:02:34 by kkamashi         ###   ########.fr       */
+/*   Updated: 2021/03/19 16:57:17 by kkamashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
-
-void		sort_stack_a_by_size_is_two(t_main *main)
-{
-	t_stack **stack_a;
-
-	stack_a = &(main->stack_a);
-	if ((*stack_a)->value < (*stack_a)->next->value)
-	{
-		return ;
-	}
-	swap_a(main);
-}
-
-void		sort_stack_a_by_size_is_three(t_main *main)
-{
-	t_stack **stack_a;
-
-	stack_a = &(main->stack_a);
-	main->top = (*stack_a)->value;
-	main->middle = (*stack_a)->next->value;
-	main->bottom = (*stack_a)->next->next->value;
-	if (main->top > main->middle && main->top < main->bottom)
-	{
-		swap_a(main);
-	}
-	else if (main->top > main->middle && main->middle > main->bottom)
-	{
-		swap_a(main);
-		reverse_rotate_a(main);
-	}
-	else if (main->top > main->middle && main->middle < main->bottom)
-	{
-		rotate_a(main);
-	}
-	else if (main->top < main->middle && main->top < main->bottom && main->middle > main->bottom)
-	{
-		swap_a(main);
-		rotate_a(main);
-	}
-	else if (main->top < main->middle && main->top > main->bottom && main->middle > main->bottom)
-	{
-		reverse_rotate_a(main);
-	}
-}
 
 void		sort_stack_a_by_size(t_main *main)
 {
@@ -62,22 +18,17 @@ void		sort_stack_a_by_size(t_main *main)
 		return ;
 	else if (main->size == 2)
 	{
-		sort_stack_a_by_size_is_two(main);
+		sort_stack_with_two_numbers(main);
 	}
 	else if (main->size == 3)
 	{
-		sort_stack_a_by_size_is_three(main);
+		sort_stack_with_three_numbers(main);
 	}
-	else if (main->size <= 5)
+	else if (main->size >= 4)
 	{
-
-	}
-	else
-	{
-
+		sort_stack_with_more_than_four_numbers(main);
 	}
 }
-
 
 int			main(int argc, char *argv[])
 {
@@ -98,14 +49,6 @@ int			main(int argc, char *argv[])
 	}
 	// SORT
 	sort_stack_a_by_size(&main);
-	// if (!is_stack_sorted(main.stack_a))
-	// {
-	// 	return (print_out_ko());
-	// }
-	// if (!is_stack_empty(main.stack_b))
-	// {
-	// 	return (print_out_ko());
-	// }
 	print_out_stack(main.stack_a, main.stack_b);
 	print_out_count(&main);
 	free_memory(&(main.stack_a), &(main.stack_b));
