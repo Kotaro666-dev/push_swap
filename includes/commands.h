ff2c4a6aeec96a1bd3275d627f8960c6135c806a
Kotaro666-dev <kotaro0726@gmail.com>
2021-03-19 13:50:48 +0900

    Modify: Modify parameter; t_stack -> t_main

--- includes/commands.h
+++ includes/commands.h
@@ -6,7 +6,7 @@
 /*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/13 21:22:16 by kkamashi          #+#    #+#             */
-/*   Updated: 2021/03/16 22:49:41 by kkamashi         ###   ########.fr       */
+/*   Updated: 2021/03/19 13:44:08 by kkamashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@
 # include "main.h"
 # include "struct.h"
 
-void	swap_a(t_stack **stack_a);
-void	swap_b(t_stack **stack_b);
+void	swap_a(t_main *main);
+void	swap_b(t_main *main);
 void	swap_a_and_b(t_main *main);
 void	push_a(t_stack **stack_a, t_stack **stack_b);
 void	push_b(t_stack **stack_a, t_stack **stack_b);
