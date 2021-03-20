e9638a53d0728224692a71941b1787a04c005998
Kotaro666-dev <kotaro0726@gmail.com>
2021-03-20 21:41:09 +0900

    Update: Update header file

--- includes/main.h
+++ includes/main.h
@@ -6,7 +6,7 @@
 /*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/11 08:00:14 by kkamashi          #+#    #+#             */
-/*   Updated: 2021/03/19 16:55:14 by kkamashi         ###   ########.fr       */
+/*   Updated: 2021/03/20 21:32:36 by kkamashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,25 @@
 
 void		sort_stack_with_two_numbers(t_main *main);
 void		sort_stack_with_three_numbers(t_main *main);
-void		sort_stack_with_more_than_four_numbers(t_main *main);
+void		sort_stack_with_small_numbers(t_main *main);
+void		sort_stack_with_many_numbers(t_main *main, int divider);
+
+/*
+** PUSH_ALL_NUMBERS_TO_STACK_B_UTILS.C
+*/
+
+int			count_step_from_top(t_stack *stack, int maximum);
+int			count_step_from_tail(t_stack *stack, int maximum);
+int			is_minimum_number_on_top(t_stack *stack);
+int			get_stack_size(t_stack *stack);
+int			find_minimum_position(t_stack *stack);
+void		put_minimum_number_on_top(t_main *main);
+
+/*
+** PUSH_ALL_NUMBERS_TO_STACK_A_UTILS.C
+*/
+
+int			count_step_to_maximum_from_head(t_stack *stack, int maximum);
+int			count_step_to_maximum_from_tail(t_stack *stack, int maximum);
 
 #endif
