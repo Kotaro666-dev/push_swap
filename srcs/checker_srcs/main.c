33c1dcdf61813e6968858cdeef7f8c48158e47c3
Kotaro666-dev <kotaro0726@gmail.com>
2021-03-21 14:53:01 +0900

    Norm: Norminette check

--- srcs/checker_srcs/main.c
+++ srcs/checker_srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/11 08:02:55 by kkamashi          #+#    #+#             */
-/*   Updated: 2021/03/21 14:23:58 by kkamashi         ###   ########.fr       */
+/*   Updated: 2021/03/21 14:52:25 by kkamashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,13 @@ int		main(int argc, char *argv[])
 	t_main main;
 
 	if (argc == 1)
-	{
 		return (0);
-	}
 	if (initialize_struct(&main, argc) == MALLOC_ERROR)
-	{
 		return (MALLOC_ERROR);
-	}
 	if (initialize_stack_a(&main, argv + 1) == ERROR)
-	{
 		return (print_out_error());
-	}
 	if (!is_every_number_unique(main.stack_a))
-	{
 		return (print_out_error());
-	}
 	if (read_and_operate_commands(&main) == ERROR)
 	{
 		return (print_out_error());
@@ -44,7 +36,6 @@ int		main(int argc, char *argv[])
 	{
 		return (print_out_ko());
 	}
-	// print_out_count(&main);
 	free_memory(&(main.stack_a), &(main.stack_b));
 	return (print_out_ok());
 }
