3c5cf3e18cddc26a68295d0ae859530f6b1747b2
Kotaro666-dev <kotaro0726@gmail.com>
2021-03-27 14:13:49 +0900

    Update: Add new algorithm to optimize sort
    - Add count_min_steps_to_target function
    - Above-mentioned function is used to compare min steps to max_number with min steps to min_number
    - This algorithm enables to reduce average numbers of times of commands from 8 to 7 on less than 10 stack size

--- srcs/push_swap_srcs/push_max_and_min_number_stack_b.c
+++ srcs/push_swap_srcs/push_max_and_min_number_stack_b.c
@@ -6,13 +6,13 @@
 /*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/27 13:49:16 by kkamashi          #+#    #+#             */
-/*   Updated: 2021/03/27 13:49:56 by kkamashi         ###   ########.fr       */
+/*   Updated: 2021/03/27 14:11:20 by kkamashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
 
-void		push_max_number_to_stack_b(t_main *main)
+static void		push_max_number_to_stack_b(t_main *main)
 {
 	int	steps_from_head;
 	int	steps_from_tail;
@@ -29,7 +29,7 @@ void		push_max_number_to_stack_b(t_main *main)
 	}
 }
 
-void		push_min_number_to_stack_b(t_main *main)
+static void		push_min_number_to_stack_b(t_main *main)
 {
 	int	steps_from_head;
 	int	steps_from_tail;
@@ -43,5 +43,40 @@ void		push_min_number_to_stack_b(t_main *main)
 	else
 	{
 		rotate_stack_and_push_b(main, steps_from_tail, FROM_TAIL);
+	}
+}
+
+static int		count_min_steps_to_target(t_main *main, int target)
+{
+	int	steps_from_head;
+	int	steps_from_tail;
+
+	steps_from_head = count_step_from_head_to_target(main, target);
+	steps_from_tail = count_step_from_tail_to_target(main, target);
+	if (steps_from_head < steps_from_tail)
+	{
+		return (steps_from_head);
+	}
+	return (steps_from_tail);
+}
+
+void			push_max_and_min_number_to_stack_b(t_main *main)
+{
+	if (count_min_steps_to_target(main, main->maximum) <
+							count_min_steps_to_target(main, main->minimum))
+	{
+		push_max_number_to_stack_b(main);
+		if (main->size > 3)
+		{
+			push_min_number_to_stack_b(main);
+		}
+	}
+	else
+	{
+		push_min_number_to_stack_b(main);
+		if (main->size > 3)
+		{
+			push_max_number_to_stack_b(main);
+		}
 	}
 }
