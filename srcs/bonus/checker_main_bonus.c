995e6829973dc29b3ee5550bd8ffe953dd087a32
Kotaro666-dev <kotaro0726@gmail.com>
2021-03-30 16:40:10 +0900

    Update: Add bonus option

--- srcs/bonus/checker_main_bonus.c
+++ srcs/bonus/checker_main_bonus.c
@@ -6,7 +6,7 @@
 /*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/27 15:20:37 by kkamashi          #+#    #+#             */
-/*   Updated: 2021/03/27 15:44:58 by kkamashi         ###   ########.fr       */
+/*   Updated: 2021/03/30 16:39:56 by kkamashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,27 +16,26 @@ int		main(int argc, char *argv[])
 {
 	t_main main;
 
-	printf("BONUS");
 	if (argc == 1)
 		return (0);
 	if (initialize_struct(&main, argc) == MALLOC_ERROR)
 		return (MALLOC_ERROR);
+	read_and_store_option(&main, argv + 1);
+	argv += main.option.is_v + main.option.is_c;
+	argc -= main.option.is_v + main.option.is_c;
+	if (argc == 1)
+		return (0);
 	if (initialize_stack_a(&main, argv + 1) == ERROR)
 		return (print_out_error(&main));
+	print_out_stack_and_clear_screen(&main);
 	if (!is_every_number_unique(main.stack_a))
 		return (print_out_error(&main));
 	if (read_and_operate_commands(&main) == ERROR)
-	{
 		return (print_out_error(&main));
-	}
 	if (!is_stack_sorted(main.stack_a))
-	{
 		return (print_out_ko(&main));
-	}
 	if (!is_stack_empty(main.stack_b))
-	{
 		return (print_out_ko(&main));
-	}
 	dispose_memory(&main);
 	return (print_out_ok());
 }
