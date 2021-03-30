c746fb2cee30d0806dbc195d5883c363714a7c4e
Kotaro666-dev <kotaro0726@gmail.com>
2021-03-30 13:07:13 +0900

    Update: Add Clear Screen constant

--- includes/constant.h
+++ includes/constant.h
@@ -6,7 +6,7 @@
 /*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/12 14:41:33 by kkamashi          #+#    #+#             */
-/*   Updated: 2021/03/21 19:37:10 by kkamashi         ###   ########.fr       */
+/*   Updated: 2021/03/27 17:11:11 by kkamashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,6 @@
 # define NOT_FOUND -1
 # define FROM_HEAD 2
 # define FROM_TAIL 3
+# define CLEAR_SCREEN "\e[1;1H\e[2J"
 
 #endif
